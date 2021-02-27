153ec6d72c8c92a2bfeff00468e663a0e2bde647
jonas99g <51866807+jonas99g@users.noreply.github.com>
2021-02-27 17:10:39 +0100

    snakeify

--- bms/pi_bms_uart.py
+++ bms/pi_bms_uart.py
@@ -1,50 +1,77 @@
 #esp: HardwareSerial BMS(1); //defining "BMS" as HardwareSerial on UART 1
 
+#Py Pi Code
+import serial
+import time
 
 
-ask_etl = bytes([0xAA, 0x09, 0x04, 0x22, 0x00, 0x23, 0x00, 0xF3, 0x1B ]) #reg:34,35; [UINT_32] / Resolution 1 s R
-ask_packv = bytes([0xAA, 0x09, 0x04, 0x24, 0x00, 0x25, 0x00, 0xF0, 0x33 ]) #reg:36,37; [FLOAT] / Resolution 1 V R
-ask_packc = bytes([0xAA, 0x09, 0x04, 0x26, 0x00, 0x27, 0x00, 0xF0, 0xEB ]) #reg:38,39; [FLOAT] / Resolution 1 A R
+ask_etl = bytes([0xAA, 0x09, 0x04, 0x22, 0x00, 0x23, 0x00, 0xF3, 0x1B]) #reg:34,35; [UINT_32] / Resolution 1 s R
+ask_packv = bytes([0xAA, 0x09, 0x04, 0x24, 0x00, 0x25, 0x00, 0xF0, 0x33]) #reg:36,37; [FLOAT] / Resolution 1 V R
+ask_packc = bytes([0xAA, 0x09, 0x04, 0x26, 0x00, 0x27, 0x00, 0xF0, 0xEB]) #reg:38,39; [FLOAT] / Resolution 1 A R
 #syspow [FLOAT] / Resolution 1 W R
-ask_mincv = bytes([0xAA, 0x09, 0x02, 0x28, 0x00, 0x60, 0x45 ]) #reg:40; [UINT_16] / Resolution 1 mV R
-ask_maxcv = bytes([0xAA, 0x09, 0x02, 0x29, 0x00, 0x81, 0xD4 ]) #reg:41; [UINT_16] / Resolution 1 mV R
+ask_mincv = bytes([0xAA, 0x09, 0x02, 0x28, 0x00, 0x60, 0x45]) #reg:40; [UINT_16] / Resolution 1 mV R
+ask_maxcv = bytes([0xAA, 0x09, 0x02, 0x29, 0x00, 0x81, 0xD4]) #reg:41; [UINT_16] / Resolution 1 mV R
 #inbalance of cells (maxcv - mincv 
-ask_soc = bytes([0xAA, 0x09, 0x04, 0x2E, 0x00, 0x2F, 0x00, 0xF5, 0x4B ]) #reg:46,47; [UINT_32] / Resolution 0.000001 % R
-ask_bmstemp = bytes([0xAA, 0x09, 0x02, 0x30, 0x00, 0x8A, 0x44 ])#reg:48; [INT_16] / Resolution 0.1 °C R
-ask_bmsstatus = bytes([0xAA, 0x09, 0x02, 0x32, 0x00, 0x8B, 0x24 ]) #reg:50 BMS Online Status [UINT_16] / 0x91-Charging, 0x92-Fully Charged, 0x93-Discharging, 0x96-Regenertion, 0x97-Idle, 0x9B-Fault R
-ask_nevents = bytes([0xAA, 0x11, 0xBF, 0x1C])#Read Tiny BMS newest Events
+ask_soc = bytes([0xAA, 0x09, 0x04, 0x2E, 0x00, 0x2F, 0x00, 0xF5, 0x4B]) #reg:46,47; [UINT_32] / Resolution 0.000001 % R
+ask_bmstemp = bytes([0xAA, 0x09, 0x02, 0x30, 0x00, 0x8A, 0x44])#reg:48; [INT_16] / Resolution 0.1 °C R
+ask_bmsstatus = bytes([0xAA, 0x09, 0x02, 0x32, 0x00, 0x8B, 0x24]) #reg:50 BMS Online Status [UINT_16] / 0x91-Charging, 0x92-Fully Charged, 0x93-Discharging, 0x96-Regenertion, 0x97-Idle, 0x9B-Fault R
+ask_nevents = bytes([0xAA, 0x11, 0xBF, 0x1C]) #Read Tiny BMS newest Events
+
+initSeq = bytes([0xAA, 0x09, 0x34])
+
+#initialize serial
+#esp: void setup() BMS.begin(115200, SERIAL_8N1, 26, 25); //rx, tx
+BMS = serial.Serial()
+
+int etl #convert seconds to minutes?
+float packv 
+float packc 
+float syspow
+float mincv
+float maxcv
+float cvinbal
+int soc
+float bmstemp
+str bmsstatus
+chr events
+
+#send ask_sequence twice
+#receive answer sequence and store it
+#check init Sequence (eg AA 09 04)
+#calculate and check CRC16 MODBUS checkbit
+#read and encode value to variable
 
 
 
 
-byte bms_registers_msg[] = {0xAA, 0x09, 0x1A, 0x24, 0x00, 0x25, 0x00, 0x26, 0x00, 0x27, 0x00, 0x28, 0x00, 0x29, 0x00, 0x2E, 0x00, 0x2F, 0x00, 0x30, 0x00, 0x32, 0x00, 0x33, 0x00, 0x34, 0x00, 0x04, 0x01, 0xA8, 0x3F};
-byte initSeq[] = {0xAA, 0x09, 0x34};
-byte rxData[52];
-float PackVoltage = 0;
-float PackCurrent = 0;
-float SystemPower = 0;
-uint16_t MinCellV = 0; // (1 mV) i+21, i+22 2 bytes
-uint32_t SOC = 0; // (0.000001 %)
-uint16_t BMSTemp = 0; // (0.1°C)
-uint16_t BMSOnline = 0; // maybe dictionary for status output 0x91-Charging, 0x92-Fully Charged, 0x93-Discharging, 0x96-Regenertion, 0x97-Idle, 0x9B-Fault
+#ESP Code
 
+#write ask_sequence
+#read UART buffer and storing in cache
+#finding start sequence by comparing
+#reading payload length and reading through length + checkbit
+#print all received data
+  byte PackVoltage_array[4] = {rx[j+5], rx[j+6], rx[j+9], rx[j+10]};
+  PackVoltage = bytesToFloat(PackVoltage_array);
+  
+  byte PackCurrent_array[4] = {rx[j+13], rx[j+14], rx[j+17], rx[j+18]};
+  PackCurrent = bytesToFloat(PackCurrent_array);
+  
+  SystemPower = PackVoltage * PackCurrent;
 
-
-void setup() {
-  Serial.begin(115200);
-  BMS.begin(115200, SERIAL_8N1, 26, 25); //rx, tx
-  //Pixhawk.begin(115200, SERIAL_8N1, 33, 32);
-  //customized message to ask for relevant registers, starting with AA, 09, Payloadlength and ending with two chck bytes calculated with crc-16
-  delay(5);
+  byte MinCellV_array[2] = {rx[j+21], rx[j+22]};
+  MinCellV = bytesToUint16(MinCellV_array); // (1 mV) i+21, i+22 2 bytes
+  MinCellV/1000)
+  
+  byte SOC_array[4] = {rx[j+29], rx[j+30], rx[j+33], rx[j+34]};
+  SOC = bytesToUint32(SOC_array); // Resolution 0.000001 %
+  (SOC/1000000)
 }
+  
 
-
-void loop()
-{
-  askRegisters();
-  //delay(1000);  
-}
-
+  
+  
+#encoding madness of C++
 float bytesToFloat(byte byte_array[4])
 {
   float float_var;
@@ -63,84 +90,6 @@ uint32_t bytesToUint32(byte byte_array[4])
   memcpy(&int_var,byte_array,4);
   return int_var;
 }
-void askRegisters()
-{
-  byte rx[156] = {};
-  int i = 0;
-  int j = 0;
-  
-  BMS.write(bms_registers_msg, 31);
-  delay(1);
-  BMS.write(bms_registers_msg, 31);
-  delay(5); 
-  
-  while(BMS.available() > 0) { //read bytes when UART buffer is not empty
-    BMS.readBytes(rx, 156); //read bytes to the rx array with the size 156
-  }
-  //finding start sequence
-  for (i = 0; i < 157; i++) {
-    byte rxCurrent[] = {rx[i],rx[i+1],rx[i+2]}; 
-    if (memcmp(rxCurrent, initSeq, 3) == 0) { //comparing the memory content of the arrays to find the starting sequence of 0xAA, 0x09; 2 bytes long; memcmp returns 0 if it matches
-      /* check for buffer overflow Serial.print("found: break at i= ");Serial.println(i);*/ j=i;break;
-    }
-  }
-  
-  //printing rxData for testing
-  int PL = (int)rx[i+2];
-  //Serial.print("PL = ");
-  //Serial.println(PL);
-  for (int n = 0; n < PL; n++) {
-    rxData[n] = rx[i];
-    //rxData[n] = rx[i+3];
-    Serial.print(rxData[n],HEX);
-    Serial.print(","); 
-    i++;
-  }
-  Serial.print("\n");
-  
-  byte PackVoltage_array[4] = {rx[j+5], rx[j+6], rx[j+9], rx[j+10]};
-  PackVoltage = bytesToFloat(PackVoltage_array);
-  /*
-  for (int k = 0; k < 4; k++) {
-    Serial.println(PackVoltage_array[k],HEX);
-  }*/
-  Serial.print("Batterypack Voltage: ");
-  Serial.print(PackVoltage);
-  Serial.println("V");
-  
-  byte PackCurrent_array[4] = {rx[j+13], rx[j+14], rx[j+17], rx[j+18]};
-  PackCurrent = bytesToFloat(PackCurrent_array);
-  /*
-  for (int k = 0; k < 4; k++) {
-    Serial.println(PackCurrent_array[k],HEX);
-  }*/
-  Serial.print("Batterypack Current: ");
-  Serial.print(PackCurrent);
-  Serial.println("A");
-  
-  SystemPower = PackVoltage * PackCurrent;
-  Serial.print("System Power: ");
-  Serial.print(SystemPower);
-  Serial.println("W");
 
-  byte MinCellV_array[2] = {rx[j+21], rx[j+22]};
-  MinCellV = bytesToUint16(MinCellV_array); // (1 mV) i+21, i+22 2 bytes
-  /*
-  for (int k = 0; k < 2; k++) {
-    Serial.println(MinCellV_array[k],HEX);
-  }*/
-  Serial.print("Minimal Cell Voltage: ");
-  Serial.print(MinCellV/1000);
-  Serial.println("V");
-  
-  float SOC;
-  byte SOC_array[4] = {rx[j+29], rx[j+30], rx[j+33], rx[j+34]};
-  SOC = bytesToUint32(SOC_array); // Resolution 0.000001 %
-  /*
-  for (int k = 0; k < 4; k++) {
-    Serial.println(SOC_array[k],HEX);
-  }*/
-  Serial.print("State of Charge: ");
-  Serial.print((SOC/1000000));
-  Serial.println("%");
-}
+
+
