f16d583e41efef4611b1035bb7dd43e850e9fc7d
jonas99g <51866807+jonas99g@users.noreply.github.com>
2021-02-28 18:41:45 +0100

    hex2int

--- bms/pi_bms_uart.py
+++ bms/pi_bms_uart.py
@@ -1,11 +1,12 @@
-
+import sys
+import os
 import serial
 import time
 from __future__ import print_function
 import paho.mqtt.publish as publish
 import psutil
 import string
-import random
+
 
 writeAPIKey = "V13QLD2JANKYVBV1"
 mqttAPIKey = "R95SKCL1DYRQNF67"
@@ -33,94 +34,57 @@ init_seq_4 = bytes([0xAA, 0x09, 0x08])
 int etl #convert seconds to minutes?
 float packv 
 float packc 
-float syspow
-float mincv
+float syspow #SystemPower = PackVoltage * PackCurrent
+float mincv #MinCellV/1000
 #float maxcv
 #float cvinbal
-int soc
+int soc #SOC/1000000
 float bmstemp
-str bmsstatus
+#str bmsstatus
 #str events
 BMS = serial.Serial(port='/dev/ttyAMA0',baudrate=115200,bytesize=serial.EIGHTBITS,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE, timeout=1,xonxoff=0,rtscts=0,dsrdtr=0,write_timeout=1,)
 
-payload = "etl=" + str(etl) + "&packv=" + str(packv) + "&packc=" + str(packc) + "&syspow=" + str(syspow) + "&mincv=" + str(mincv) + "&soc=" + str(soc) + "&bmstemp=" + str(bmstemp) + "&bmsstatus=" + str(bmsstatus)
-try:
-  publish.single(topic, payload, hostname=mqttHost, transport=tTransport, port=tPort,auth={'username':mqttUsername,'password':mqttAPIKey})
+while 1:
+  etl = int(read_status_4(ask_etl))
+
+  payload = "etl=" + str(etl) + "&packv=" + str(packv) + "&packc=" + str(packc) + "&syspow=" + str(syspow) + "&mincv=" + str(mincv) + "&soc=" + str(soc) + "&bmstemp=" + str(bmstemp) """+ "&bmsstatus=" + str(bmsstatus""")
+  try:
+    publish.single(topic, payload, hostname=mqttHost, transport=tTransport, port=tPort,auth={'username':mqttUsername,'password':mqttAPIKey})
 
 
-def read_status(ask_seq,init_seq,output_variable,pl_len) {
+def read_status_2(ask_seq) {
   bms_dump = bytes([])
   msg = bytes ([])
+  pl_len = 4
   BMS.reset_input_buffer()
   BMS.reset_output_buffer()
   BMS.write(ask_seq)
   BMS.flush()
   BMS.write(ask_seq)
   BMS.flush()
-  bms_dump = BMS.read_until(init_seq)
-  msg = BMS.read((pl_len+5))
-  
-  return output_variable
+  bms_dump = BMS.read_until(init_seq_2)
+  msg = BMS.read(size=(pl_len+5))
+  return msg
+}
+def read_status_4(ask_seq) {
+  bms_dump = bytes([])
+  msg = bytes ([])
+  pl_len = 8
+  BMS.reset_input_buffer()
+  BMS.reset_output_buffer()
+  BMS.write(ask_seq)
+  BMS.flush()
+  BMS.write(ask_seq)
+  BMS.flush()
+  bms_dump = BMS.read_until(init_seq_4)
+  msg = BMS.read(size=(pl_len+5))
+  return msg
 }
 
 
 
-
-#send ask_sequence twice
-#receive answer sequence and store it
-#check init Sequence (eg AA 09 04)
-#calculate and check CRC16 MODBUS checkbit
-#read and encode value to variable
-
-
-
-
-#ESP Code
-
-#write ask_sequence
-#read UART buffer and storing in cache
-#finding start sequence by comparing
-#reading payload length and reading through length + checkbit
-#print all received data
-  byte PackVoltage_array[4] = {rx[j+5], rx[j+6], rx[j+9], rx[j+10]};
-  PackVoltage = bytesToFloat(PackVoltage_array);
-  
-  byte PackCurrent_array[4] = {rx[j+13], rx[j+14], rx[j+17], rx[j+18]};
-  PackCurrent = bytesToFloat(PackCurrent_array);
-  
-  SystemPower = PackVoltage * PackCurrent;
-
-  byte MinCellV_array[2] = {rx[j+21], rx[j+22]};
-  MinCellV = bytesToUint16(MinCellV_array); // (1 mV) i+21, i+22 2 bytes
-  MinCellV/1000)
-  
-  byte SOC_array[4] = {rx[j+29], rx[j+30], rx[j+33], rx[j+34]};
-  SOC = bytesToUint32(SOC_array); // Resolution 0.000001 %
-  (SOC/1000000)
-}
-  
-
-  
-  
-#encoding madness of C++
-float bytesToFloat(byte byte_array[4])
-{
-  float float_var;
-  memcpy(&float_var,byte_array,4);
-  return float_var;
-}
-uint16_t bytesToUint16(byte byte_array[2])
-{
-  uint16_t int_var;
-  memcpy(&int_var,byte_array,2);
-  return int_var;
-}
-uint32_t bytesToUint32(byte byte_array[4])
-{
-  uint32_t int_var;
-  memcpy(&int_var,byte_array,4);
-  return int_var;
-}
-
-
-
+"""send ask_sequence twice
+receive answer sequence and store it
+check init Sequence (eg AA 09 04)
+calculate and check CRC16 MODBUS checkbit
+read and encode value to variable"""
